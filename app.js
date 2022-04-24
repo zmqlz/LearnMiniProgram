02ad6b848e0985cb20b451d7b88d215207d8780e
zmqlz <184155892@qq.com>
2022-04-24 17:19:58 +0800

    初始化项目

---
+++ app.js
@@ -0,0 +1,35 @@
+// 注册小程序示例
+App({
+
+  // 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+  // 也有一个 options 参数
+  onLaunch: function (options) {
+    // wx.getUserInfo({
+    //   success(res) {
+    //     console.log(res);
+    //   }
+    // })
+    console.log(options);
+  },
+
+//  当小程序启动，或从后台进入前台显示，会触发 onShow
+  onShow: function (options) {
+    // 打印小程序的进入场景
+    console.log(options);
+  },
+
+  //  当小程序从前台进入后台，会触发 onHide
+  onHide: function () {
+
+  },
+
+  // 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+  onError: function (msg) {
+
+  },
+  // 设置全局变量
+  globalData:{
+    name:"罗颖",
+    gender:"女"
+  }
+})
